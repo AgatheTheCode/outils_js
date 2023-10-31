2314c8bcb8ff772d5a6d69367f87c88b2c5aff9d
Agathe <agathe.dufour.marin@gmail.com>
2023-10-31 10:04:57 +0100

    Update outils.js
    + eventhandler
    + docstrings

--- outils.js
+++ outils.js
@@ -1,5 +1,27 @@
-//requête ajax
-function makeRequest(url, type, action) {
+
+/**
+ * Adds event listener to the target element.
+ * If the clicked element is not the target, it executes the specified action.
+ * @param {HTMLElement} target - The element to attach the event listener to.
+ * @param {Function} action - The action to execute when the click event is triggered.
+ * @param {HTMLElement} source - The element to listen for click events on. Defaults to the document.
+ * @param delay delay - The delay in milliseconds between each execution of the action.
+ * @returns {HTMLElement} - The target element.
+ */
+function eventHandler(target, action, source = document, delay = 50) {
+    source.addEventListener("click", throttle(function (e) {
+        action(e);
+    }, delay));
+    return target;
+}
+
+
+/**
+ * Makes an AJAX request to the specified URL with the given type and action.
+ * @param {string} url - The URL to send the request to.
+ * @param {string} type - The type of request (e.g. "GET", "POST").
+ * @param {string} action - The action to be performed on the server.
+ */function makeRequest(url, type, action) {
     let xhr = new XMLHttpRequest();
     if (!xhr) {
         return false;
@@ -10,7 +32,12 @@ function makeRequest(url, type, action) {
     xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
     xhr.send(action);
 }
-//délai
+/**
+ * Throttles the execution of a callback function.
+ * @param {function} callback - The function to be throttled.
+ * @param {number} delay - The delay in milliseconds.
+ * @returns {function} - The throttled function.
+ */
 function throttle(callback, delay) {
     let lastCallTime = 0;
     let timerId;
@@ -31,7 +58,13 @@ function throttle(callback, delay) {
 }
 
 //attendre un élément
-
+/**
+ * Waits for an element to be added to the DOM.
+ *
+ * @param {string} selector - The CSS selector of the element to wait for.
+ * @param {HTMLElement} [parent=document] - The parent element to search for the target element in. Defaults to the document.
+ * @return {Promise<HTMLElement>} A promise that resolves with the target element once it is added to the DOM.
+ */
 function waitForElm(selector, parent = document) {
     return new Promise((resolve) => {
         const target = parent.querySelector(selector);
